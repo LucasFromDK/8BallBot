cf216534ae92b7ef4f855724444e8e90a49729b2
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-03-27 14:20:40 +0200

    8Ball Feature

--- main.js
+++ main.js
@@ -24,7 +24,10 @@ client.on(Events.MessageCreate, onMessage);
 
 function onMessage(msg) {
     if (msg.author.bot) return;
-    if(msg.content.toLowerCase().includes("hej")) {
-        msg.reply("Hej med dig 😃")
+    if(msg.content.toLowerCase().includes("!ask")) {
+        const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "	Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don’t count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+        let response = responses[Math.floor(Math.random() * responses.length)]
+        msg.reply(response)
+        console.log("Random Answer: " + response)
     }
 }
