af264ffde3d8a312847ca6d7efeb2be7e9e65fea
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-03-27 17:05:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function onMessage(msg) {
     if (msg.author.bot) return;
     if(msg.content.toLowerCase().includes("!help")) {
         msg.reply("**Commands:**\n<@1088016851403026462> !ask (Question) \nMore Commands Coming Soon")
-    } else if(msg.content.toLowerCase().includes("!help")) {
+    } else if(msg.content.toLowerCase().includes("!ask")) {
         const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "	Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don’t count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
         let response = responses[Math.floor(Math.random() * responses.length)]
         msg.reply(response)
