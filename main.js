1300b5d72b1d0251adcb92a9c8a1dcf79791bf87
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-03-27 17:00:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 const { Client, Collection, Events, GatewayIntentBits, ActivityType} = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages], partials: ['MESSAGE', 'CHANNEL']});
-client.commands = new Collection()
 
 require('dotenv').config(); //Loads .env
 
@@ -23,11 +22,14 @@ client.on(Events.MessageCreate, onMessage);
 
 function onMessage(msg) {
     if (msg.author.bot) return;
-    if(msg.content.toLowerCase().includes("!ask")) {
+    if(msg.content.toLowerCase().includes("!help")) {
+        msg.reply("**Commands:**\n<@1088016851403026462> !ask (Question) \nMore Commands Coming Soon")
+    } else if(msg.content.toLowerCase().includes("!help")) {
         const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "	Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don’t count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
         let response = responses[Math.floor(Math.random() * responses.length)]
         msg.reply(response)
         console.log("User Asked: " + msg.content +", Random Answer: " + response)
+
     } else if(msg.content.toLowerCase().includes("!roll")) {
         args = msg.split(" +/") //Converts to Arguments
         args.shift().toLowerCase() //Makes it lowercase
