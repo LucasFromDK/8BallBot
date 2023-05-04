c4fa0fa1d0ab6dc97c2bebd04e5db2e79320b032
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-05-04 19:57:23 +0200

    Update

--- commands/default/joke.js
+++ commands/default/joke.js
@@ -1,22 +1,27 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-	.setName('joke')
-	.setDescription('Tells you a joke.'),
+  data: new SlashCommandBuilder()
+    .setName('joke')
+    .setDescription('Tells you a joke.'),
 
-	async execute(interaction) {
-		const responses = ["Yesterday I saw a guy spill all his Scrabble letters on the road, I asked him: Whats the word on the street?"]
-		const response = responses[Math.floor(Math.random() * responses.length)]
+  async execute(interaction) {
+    const response = await axios.get('https://official-joke-api.appspot.com/random_joke');
+    const jokeData = response.data;
+    const setup = jokeData.setup;
+    const punchline = jokeData.punchline;
 
-		const embed = new EmbedBuilder()
-			.setColor(0x5865F2)
-			.setTitle("Joke")
-			.setFooter({ text: 'So funny, right?', iconURL: 'https://cdn.discordapp.com/avatars/1088016851403026462/06bd8b265d56024aa1e002e30f25a79a.png?size=1024' })
-			.addFields(
-				{ name: ' ', value: response },
-			);
+    const embed = new EmbedBuilder()
+      .setColor(0x5865F2)
+      .setTitle('Joke')
+      .setDescription(setup)
+      .addFields(
+        { name: '\n', value: '\n' },
+        { name: 'Punchline', value: punchline }
+      )
+      .setFooter({ text: 'So funny, right?', iconURL: 'https://cdn.discordapp.com/avatars/1088016851403026462/06bd8b265d56024aa1e002e30f25a79a.png?size=1024' })
 
-		interaction.reply({ embeds: [embed] });
-	},
+    await interaction.reply({ embeds: [embed] });
+  },
 };
